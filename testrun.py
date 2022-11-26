b6a797520c49f07224fa0065b997b21e27cb954f
Andrew Farrell <atf8760@gmail.com>
2022-11-25 23:22:30 -0500

    remove virtual env from remote

--- testrun.py
+++ testrun.py
@@ -1,3 +1,14 @@
 import gym_examples
 import gym
-env = gym.make('gym_examples/GridWorld-v0')
+
+
+# set up to run on main
+def main():
+    env = gym.make('gym_examples/GridWorld-v0')
+    state, *_ = env.reset()
+    temrinated = False
+    while not 
+
+if __name__ == '__main__':
+    main
+
