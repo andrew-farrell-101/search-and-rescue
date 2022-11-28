593732f94649207b8aeeed75db3d532a35ad32d8
Monish Reddy Kotturu <monishkotturu@gmail.com>
2022-11-27 21:30:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup
 setup(
     name="gym_examples",
     version="0.0.1",
-    install_requires=["gym==0.26.0", "pygame==2.1.0"],
+    install_requires=[
+        "gym==0.26.0",
+        "pygame==2.1.0",
+        "numpy==1.23.5",
+        "matplotlib==3.5.2",
+    ],
 )
